c424ef0f94f9d31f700cdef6a4461d72df62c369
German <german.casanas@uner.edu.ar>
2023-03-08 10:56:00 -0300

    primer commit del proyecto

---
+++ cv/js/function.js
@@ -0,0 +1,10 @@
+//console.log("probando js");
+/**Mostramos datos de contacto capturando evento clic*/
+document.getElementById("mail").onclick = function(){
+    document.getElementById("mail").innerHTML="antonio.rojas@smail.com";
+}
+document.getElementById("tel").onclick = function(){
+    document.getElementById("tel").innerHTML="3982-12345678";
+}
+
+
